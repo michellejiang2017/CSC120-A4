f94cb49c0e9a2f382f9dd00f357ba901f53984b7
Michelle Jiang <mhjiang@smith.edu>
2025-09-30 22:34:22 -0400

    completed passenger class edited car class print manifest to fix issues

--- Car.java
+++ Car.java
@@ -5,10 +5,10 @@ public class Car implements CarRequirements {
     // passengers assoc 
     // train agg
 
-    private ArrayList<String> passenger; 
+    private ArrayList<Passenger> passenger; 
     private int carCapacity;
     
-    public Car(ArrayList<String> passenger, int carMaxCapacity) {
+    public Car(ArrayList<Passenger> passenger, int carMaxCapacity) {
         this.passenger = passenger; 
         this.carCapacity = carMaxCapacity; 
     }
@@ -21,5 +21,31 @@ public class Car implements CarRequirements {
         return carCapacity - passenger.size(); 
     }
 
-    
+    public Boolean addPassenger(Passenger p) {
+        if (seatsRemaining() > 0 && passenger.contains(p)) {
+            passenger.add(p); 
+            return true; 
+        } else { 
+            return false; 
+        }
+    }
+
+    public Boolean removePassenger(Passenger p) {
+        if (seatsRemaining() > 0 && passenger.contains(p)) {
+            passenger.remove(p); 
+            return true; 
+        } else { 
+            return false; 
+        }
+    }
+
+    public void printManifest() {
+        if (passenger.size() > 0) { 
+            for (int i=0; i<passenger.size(); i++) {
+                System.out.println(passenger.get(i).getName());
+            }
+        } else {
+            System.out.println("This car is EMPTY.");
+        }
+    }
 }
--- Passenger.java
+++ Passenger.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Passenger {
     //The Passenger class (Passenger.java) will store information about an individual passenger
     // assoc cars & train 
@@ -5,5 +7,33 @@ public class Passenger {
 
     public Passenger(String name) {
         this.name = name;
+    }
+
+    public String getName() { 
+        return this.name; 
+    }
+
+    public void boardCar(Car c) {
+        if (c.addPassenger(this)) {
+            c.addPassenger(this); // does this make sense? 
+        } 
+    }
+
+    public void getOffCar(Car c) {
+        if (c.removePassenger(this)) { 
+            c.removePassenger(this); 
+        }
+    }
+
+    public static void main(String[] args) {
+        Passenger passenger = new Passenger("Alice"); 
+        Passenger passenger2 = new Passenger("Beatrice"); 
+        ArrayList<Passenger> passengers = new ArrayList<>();
+        passengers.add(passenger); 
+        passengers.add(passenger2);
+        Car myCar = new Car(passengers, 100); 
+        myCar.seatsRemaining(); 
+        myCar.getCapacity(); 
+        myCar.printManifest();
     }
 }
